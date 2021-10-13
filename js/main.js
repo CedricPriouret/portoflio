924ecda3269d0a266132036baa17104b206b886f
Cédric <cedric.priouret@gmail.com>
2021-10-13 15:00:41 +0200

    add remove menu burger on click

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* SHOW MENU burger */
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/* ACTIVE AND REMOVE MENU BURGER */
+
+const navLink = document.querySelectorAll(".nav_link");
+
+function linkAction() {
+  // Active Link
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
+  // Remove Menu on click
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
+}
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
